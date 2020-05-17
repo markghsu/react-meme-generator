64ced63f3d5074d2c19044a6307288c29a77d847
Mark Hsu <mark.g.hsu@gmail.com>
2020-05-16 22:52:19 -0700

    Add live editing functionality

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -11,10 +11,12 @@ class MemeGenerator extends React.Component {
             image: '',
             imageList:[],
             loading: true,
-            selected: null
+            selected: 0,
+            url:''
         }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleOnline = this.handleOnline.bind(this);
     }
     componentDidMount() {
         fetch('https://api.imgflip.com/get_memes')
@@ -25,7 +27,9 @@ class MemeGenerator extends React.Component {
             if(!data.success) throw data.error_message;
             this.setState({
                 imageList: data.data.memes,
-                loading: false
+                loading: false,
+                image: data.data.memes[0].url,
+                selected: 0
             });
         })
         .catch((err) => {
@@ -34,10 +38,22 @@ class MemeGenerator extends React.Component {
     }
     handleSubmit(evt) {
         evt.preventDefault();
-        const num = Math.floor(Math.random()*100);
-        console.log(num);
+        const num = Math.floor(Math.random()*this.state.imageList.length);
+
+        this.setState({
+            image: this.state.imageList[num].url,
+            selected: num 
+        });
+    }
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        });
+    }
+    handleOnline(evt) {
+        evt.preventDefault();
         const params = {};
-        params['template_id'] = this.state.imageList[num]['id'];
+        params['template_id'] = this.state.imageList[this.state.selected]['id'];
         params['username'] = process.env.REACT_APP_USERNAME;
         params['password'] = process.env.REACT_APP_PASSWORD;
         params['text0'] = this.state.topText;
@@ -51,20 +67,12 @@ class MemeGenerator extends React.Component {
         })
         .then((data) => {
             if(!data.success) throw data.error_message;
-            console.log(data);
             this.setState({
-                image: data.data.url,
-                loading: false,
-                selected: num 
-            });
+                url: data.data.url
+            })
         })
         .catch((err) => {
             console.error(err);
-        });
-    }
-    handleChange(evt) {
-        this.setState({
-            [evt.target.name]: evt.target.value
         });
     }
     render() {
@@ -92,10 +100,16 @@ class MemeGenerator extends React.Component {
                         value={bottomText}
                         />
                     </label>
-                    <button>Generate!</button>
+                    <button>Randomize!</button>
+                    <button onClick={this.handleOnline}>Create Online</button>
                 </form>
                 )}
-                {image && <img src={this.state.image} alt={this.state.imageList[this.state.selected]['name']} />}
+                <a href={this.state.url}>{this.state.url}</a>
+                <div className="meme">
+                    {image && <img className="meme-image" src={image} alt={this.state.imageList[this.state.selected]['name']} />}
+                    <h2 className="topText">{topText}</h2>
+                    <h2 className="bottomText">{bottomText}</h2>
+                </div>
             </div>
         );
     }
